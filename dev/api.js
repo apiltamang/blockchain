662246f7320f2604ff6bd007470641fad155b4d1
Apil Tamang <apil.tamang@gmail.com>
2022-10-07 15:43:03 -0400

    done creating the mine endpoint

---
+++ dev/api.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+const parser = require('body-parser');
+const Blockchain = require('./blockchain');
+const {v1: uuidv1} = require('uuid');
+const bitcoin = new Blockchain();
+
+const nodeAddress = uuidv1().split('-').join('');
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: false }));
+
+app.get('/blockchain', function (req, res) {
+    res.send(bitcoin);
+});
+
+app.post('/transaction', function (req, res) {
+    console.log("request: ", JSON.stringify(req.body));
+    const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+    res.json({ note: `transaction will be added in block ${blockIndex}`});
+});
+
+app.get('/mine', function (req, res) {
+    const prevBlock = bitcoin.getLastBlock();
+    const prevHash = prevBlock['hash'];
+
+    const currData = {
+        transactions: bitcoin.pendingTransactions,
+        index: prevBlock['index'] + 1
+    };
+
+    const nonce = bitcoin.proofOfWork(prevHash, currData);
+    const currHash = bitcoin.hashBlock(prevHash, currData, nonce);
+
+    bitcoin.createNewTransaction(12.5, "00", nodeAddress);
+    const newBlock = bitcoin.createNewBlock(nonce, prevHash, currHash);
+    res.json({
+        note: 'New block mined successfully',
+        block: newBlock
+    })
+});
+
+app.listen(3000, function () {
+    console.log('listening on port 3000...');
+});
