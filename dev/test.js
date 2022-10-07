6c5d9cda7c08f6e66e4e45e76517cc574b1dd18b
Apil Tamang <apil.tamang@gmail.com>
2022-10-07 05:39:41 -0400

    kickoff ... created basic blockchain prototype

---
+++ dev/test.js
@@ -0,0 +1,8 @@
+const Blockchain = require('./blockchain');
+const bitcoin = new Blockchain();
+
+bitcoin.createNewBlock(293749, '098ADFADF', 'ADSF@#$#');
+bitcoin.createNewBlock(83452, 'ADF@#$AF', 'ADFA3454');
+bitcoin.createNewBlock(83452, 'SFGSGF3243', 'CBDFGdf@#$');
+
+console.log(bitcoin);
---
+++ dev/testTransactions.js
@@ -0,0 +1,15 @@
+const Blockchain = require('./blockchain');
+const bitcoin = new Blockchain();
+
+bitcoin.createNewBlock(293749, '098ADFADF', 'ADS12345');
+bitcoin.createNewTransaction(100, 'AlexAddressABC', 'JenAddressXYZ');
+bitcoin.createNewBlock(234810, 'OAJDV654356', '097ACGAD');
+
+bitcoin.createNewTransaction(300, 'JenAddressABC', 'SamAddressXYZ');
+bitcoin.createNewTransaction(400, 'JenAddressABC', 'SamAddressXYZ');
+bitcoin.createNewTransaction(500, 'JenAddressABC', 'SamAddressXYZ');
+bitcoin.createNewBlock(345908, 'YAJXCV123', 'FJXFG29458');
+
+
+console.log(bitcoin.chain[2]);
+
